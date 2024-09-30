051319153b995454765a5a48acb7adbb482c0354
DESKTOP-6U36JVL\leesk <imskyi950228@gmail.com>
2024-10-01 00:26:46 +0900

    9/30 다시 자바 개념 다지기

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1");// hello java1을 출력합니다.
+        //System.out.println("hello java2"); 한줄주석
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+
+         */
+    }
+}
---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {//클래스범위시작
+
+    public static void main(String[] args){//메서드번위시작
+        System.out.println("hello java");//값을 콘솔에 출력하는 기능
+
+    }
+}
